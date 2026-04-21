c932451cc1e12d00e5d8e2da5bae5f4686eed5aa
DenisaDumitrascu23 <ddenisaddumitrascu4@gmail.com>
2026-04-21 08:59:18 +0300

    Laboratorul 7

---
+++ src/main/java/lab7/forms/Form.java
@@ -0,0 +1,40 @@
+package lab7.forms;
+
+public class Form {
+    private static int counter;
+    private String color;
+
+    public Form() {
+        counter++;
+        color = "white";
+    }
+
+    public Form(String color) {
+        counter++;
+        this.color = color;
+    }
+
+    public static String getInstanceCount() {
+        return String.valueOf(counter);
+    }
+
+    public float getArea() {
+        return 0;
+    }
+
+    public String toString() {
+        return "This form has the color " + color;
+    }
+
+    public boolean equals(Object obj) {
+        if (obj instanceof Form) {
+            Form f = (Form) obj;
+            if (this.color == null && f.color == null) {
+                return true;
+            } else if (this.color != null && f.color != null && this.color.compareTo(f.color) == 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
